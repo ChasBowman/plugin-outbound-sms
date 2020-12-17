5e0e086d73b815ef4752cf1ea9875691d22abed6
ChasBowman <42145200+ChasBowman@users.noreply.github.com>
2020-12-17 16:06:07 -0500

    switch from fetch to client

--- src/functions/flex-sms-task.js
+++ src/functions/flex-sms-task.js
@@ -11,320 +11,256 @@
 
     ACCESS CONTROL: Uncheck valid Twilio signature
 */
-const Twilio = require('twilio');
-const nodeFetch = require('node-fetch');
-const { URLSearchParams } = require('url');
-const uuidv1 = require('uuid/v1');
-const { Base64 } = require('js-base64');
-const TokenValidator = require('twilio-flex-token-validator').functionValidator;
+const Twilio = require('twilio')
+const nodeFetch = require('node-fetch')
+const { URLSearchParams } = require('url')
+const { v1: uuidv1 } = require('uuid')
+const { Base64 } = require('js-base64')
+const TokenValidator = require('twilio-flex-token-validator').functionValidator
 
+exports.handler = TokenValidator(async function (context, event, callback) {
+  const client = new Twilio(context.ACCOUNT_SID, context.AUTH_TOKEN)
 
-exports.handler = TokenValidator(async function(context, event, callback) {
-console.log("--- Starting ---");
+  const verifyEventProps = (event) => {
+    const result = {
+      success: false,
+    }
 
-const client = new Twilio(context.ACCOUNT_SID, context.AUTH_TOKEN);
+    console.log(event)
+    const { fromNumber, toName, toNumber, workerUri, message } = event
 
-console.log("Token Validated");
-console.log(event.toNumber);
-console.log(event.fromNumber);
-console.log(event.toName);
-console.log(event.message);
-console.log(event.workerUri);
+    if (!fromNumber) {
+      result.message = "Missing 'fromNumber' in request body"
+    } else if (!toName) {
+      result.message = "Missing 'toName' in request body"
+    } else if (!toNumber) {
+      result.message = "Missing 'message' in request body"
+    } else if (!message) {
+      result.message = "Missing 'toNumber' in request body"
+    } else {
+      result.success = true
+    }
 
-const verifyEventProps = (event) => {
-  const result = {
-    success: false,
-  };
-
-  console.log(event);
-  const { fromNumber, toName, toNumber, workerUri } = event;
-
-  if (!fromNumber) {
-    result.message = "Missing 'fromNumber' in request body";
-  } else if (!toName) {
-    result.message = "Missing 'toName' in request body";
-  } else if (!toNumber) {
-    result.message = "Missing 'toNumber' in request body";
-  } else {
-    result.success = true;
+    return result
   }
 
-  return result;
-};
+  const getFlexFlow = async (fromNumber) => {
+    try {
+      flexFlows = await client.flexApi.flexFlow.list()
 
-const getFlexFlow = (context, fromNumber) => new Promise(async (resolve, reject) => {
-  const flexFlowsApi = 'https://flex-api.twilio.com/v1/FlexFlows';
+      if (!flexFlows || flexFlows.length === 0) {
+        console.error('No Flex flows returned from fetch request')
+        return null
+      }
 
-  console.log(`Finding Flex Flow matching ${fromNumber} with integration_type of task`);
-  let fetchResponse;
-  try {
-    fetchResponse = await nodeFetch(flexFlowsApi, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Basic ${Base64.encode(`${context.ACCOUNT_SID}:${context.AUTH_TOKEN}`)}`,
-      },
-    });
-  } catch (error) {
-    console.error('Error fetching Flex Flows.', error);
-    return reject(error);
-  }
-  let jsonResponse;
-  try {
-    jsonResponse = await fetchResponse.json();
-  } catch (error) {
-    console.error('Error converting fetch response to JSON.', error);
-    return reject(error);
-  }
-
-  const flexFlows = jsonResponse && jsonResponse.flex_flows;
-  if (!flexFlows || flexFlows.length === 0) {
-    console.error('No Flex flows returned from fetch request');
-    return resolve();
-  }
-
-  let flexFlow;
-  for (let i = 0; i < flexFlows.length; i++) {
-    const flow = flexFlows[i];
-    if (flow.contact_identity === fromNumber && flow.integration_type === 'task') {
-      flexFlow = flow;
-      break;
+      let flexFlow
+      for (let i = 0; i < flexFlows.length; i++) {
+        const flow = flexFlows[i]
+        console.log(JSON.stringify(flow))
+        if (flow.contactIdentity === fromNumber && flow.integrationType === 'task') {
+          flexFlow = flow
+          break
+        }
+      }
+      console.log('found flex flex', JSON.stringify(flexFlow))
+      return flexFlow
+    } catch (error) {
+      console.error('Error creating chat channel.', error)
+      return error
     }
   }
-  return resolve(flexFlow);
-});
 
-const createChatChannelWithTask = (
-  context, flexFlowSid, identity, toNumber, toName, fromNumber, workerUri
-) => new Promise(async (resolve, reject) => {
-  const flexChannelsApi = 'https://flex-api.twilio.com/v1/Channels';
-
-  const urlParams = new URLSearchParams();
-  urlParams.append('FlexFlowSid', flexFlowSid);
-  urlParams.append('Target', toNumber);
-  urlParams.append('Identity', identity);
-  urlParams.append('ChatUserFriendlyName', toName);
-  urlParams.append('ChatFriendlyName', `SMS${toNumber}`);
-  const taskAttributes = {
-    to: fromNumber,
-    direction: 'outbound',
-    name: toName,
-    from: toNumber,
-    targetWorker: workerUri,
-    autoAnswer: true, 
-    taskIntent: `SMS Chat to ${toName}`,
-  };
-  urlParams.append('TaskAttributes', JSON.stringify(taskAttributes));
-
-  let fetchResponse;
-  try {
-    fetchResponse = await nodeFetch(flexChannelsApi, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${Base64.encode(`${context.ACCOUNT_SID}:${context.AUTH_TOKEN}`)}`,
-      },
-      body: urlParams,
-    });
-  } catch (error) {
-    console.error('Error creating chat channel.', error);
-    return reject(error);
-  }
-  let jsonResponse;
-  try {
-    jsonResponse = await fetchResponse.json();
-  } catch (error) {
-    console.error('Error converting fetch response to JSON.', error);
-    return reject(error);
+  const createChatChannelWithTask = async (
+    flexFlowSid,
+    identity,
+    toNumber,
+    toName,
+    fromNumber,
+    workerUri,
+  ) => {
+    const taskAttributes = {
+      to: fromNumber,
+      direction: 'outbound',
+      name: toName,
+      from: toNumber,
+      targetWorker: workerUri,
+      autoAnswer: true,
+      taskIntent: `SMS Chat to ${toName}`,
+    }
+    try {
+      const newChatChannel = await client.flexApi.channel.create({
+        flexFlowSid: flexFlowSid,
+        identity: identity,
+        chatUserFriendlyName: toName,
+        chatFriendlyName: `SMS${toNumber}`,
+        target: toNumber,
+        taskAttributes: JSON.stringify(taskAttributes),
+      })
+      console.log('created chat channel', JSON.stringify(newChatChannel))
+      return newChatChannel
+    } catch (error) {
+      console.error('Error creating chat channel.', JSON.stringify(error))
+      return error
+    }
   }
 
-  return resolve(jsonResponse);
-});
-
-const createMessage = (
-  context, chatChannelSid, toNumber, toName, fromNumber, workerUri, message
-) => new Promise(async (resolve, reject) => {
-  // const client = new Twilio(context.ACCOUNT_SID, context.AUTH_TOKEN);
-  //const chatClient = client.chat.services(context.TWILIO_CHAT_SERVICE_SID);
-  console.log("Adding Message");
-  let chatMessage;
-  try {
-  const messageResources = { "from": "System", "body": message }
-    chatMessage = await client.chat.services(context.TWILIO_CHAT_SERVICE_SID)
-      .channels(chatChannelSid)
-      .messages
-      .create(messageResources)
-      .then(message => console.log(message.sid));
- 
-  } catch (error) {
-    console.error('Error adding message.', error);
-    return reject(error);
+  const createChatMessage = async (chatServiceSid, chatChannelSid, body) => {
+    console.log(`running createChatMessage ${chatChannelSid} ${chatServiceSid} ${body}`)
+    const messageResources = { body, xTwilioWebhookEnabled: true }
+    try {
+      const chatMessage = await client.chat
+        .services(chatServiceSid)
+        .channels(chatChannelSid)
+        .messages.create(messageResources)
+      console.log('sent message: ' + JSON.stringify(chatMessage))
+      return chatMessage
+    } catch (error) {
+      console.error('Error adding message.', error)
+      return error
+    }
   }
 
-  return resolve(createMessage);
-});
-
-const createProxySession = (
-  context, chatChannelSid, toNumber, toName, fromNumber, workerUri
-) => new Promise(async (resolve, reject) => {
-  // const client = Twilio(context.ACCOUNT_SID, context.AUTH_TOKEN);
-  const proxyClient = client.proxy.services(context.TWILIO_PROXY_SERVICE_SID);
-
-  let proxySession;
-  try {
-    const participants = [
-      {
-        Identifier: toNumber,
-        ProxyIdentifier: fromNumber,
-        FriendlyName: toName,
-      }, {
-        Identifier: chatChannelSid,
-        ProxyIdentifier: fromNumber,
-        FriendlyName: toName,
-      },
-    ];
-    proxySession = await proxyClient.sessions.create({
-      uniqueName: chatChannelSid,
-      mode: 'message-only',
-      participants: JSON.stringify(participants),
-    });
-  } catch (error) {
-    console.error('Error creating proxy session.', error);
-    return reject(error);
+  const createProxySession = async (
+    proxyServiceSid,
+    chatChannelSid,
+    toNumber,
+    toName,
+    fromNumber,
+  ) => {
+    try {
+      const participants = [
+        {
+          Identifier: toNumber,
+          ProxyIdentifier: fromNumber,
+          FriendlyName: toName,
+        },
+        {
+          Identifier: chatChannelSid,
+          ProxyIdentifier: fromNumber,
+          FriendlyName: toName,
+        },
+      ]
+      proxySession = await client.proxy.services(proxyServiceSid).sessions.create({
+        uniqueName: chatChannelSid,
+        mode: 'message-only',
+        participants: JSON.stringify(participants),
+      })
+      console.log('created proxy session', JSON.stringify(proxySession))
+      return proxySession
+    } catch (error) {
+      console.error('Error creating proxy session.', error)
+      return reject(error)
+    }
   }
 
-  return resolve(proxySession);
-});
-
-console.log('Received event with properties:');
+  console.log('Received event with properties:')
   Object.keys(event).forEach((key) => {
-    console.log(`--${key}:`, event[key]);
-  });
+    console.log(`--${key}:`, event[key])
+  })
 
-  const response = new Twilio.Response();
-  response.appendHeader('Access-Control-Allow-Origin', '*');
-  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
-  response.appendHeader('Content-Type', 'application/json');
-  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+  const response = new Twilio.Response()
+  response.appendHeader('Access-Control-Allow-Origin', '*')
+  response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET')
+  response.appendHeader('Content-Type', 'application/json')
+  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type')
 
-  const eventCheck = verifyEventProps(event);
+  const eventCheck = verifyEventProps(event)
   if (!eventCheck.success) {
-    console.log('Event property check failed.', eventCheck.message);
-    response.setStatusCode(400);
-    response.setBody({ status: 400, message: eventCheck.message });
-    return callback(null, response);
+    console.log('Event property check failed.', eventCheck.message)
+    response.setStatusCode(400)
+    response.setBody({ status: 400, message: eventCheck.message })
+    return callback(null, response)
   }
 
-  const { fromNumber, toName, toNumber, workerUri } = event;
-
-  let flexFlow;
+  const { fromNumber, toName, toNumber, workerUri } = event
+  const responseBody = {}
+  let flexFlow
   try {
-    flexFlow = await getFlexFlow(context, fromNumber);
+    flexFlow = await getFlexFlow(fromNumber)
   } catch (error) {
-    response.setStatusCode(error && error.status);
-    response.setBody(error);
-    return callback(null, response);
-  }
-  if (!flexFlow) {
-    response.setStatusCode(500);
-    response.setBody({ message: 'Unable to find matching Flex Flow' });
-    return callback(null, response);
+    response.setStatusCode(error && error.status)
+    responseBody.error = error
+    response.setBody(responseBody)
+    return callback(null, response)
   }
 
-  const chatServicesSid = flexFlow.chat_service_sid;
-  const flexFlowSid = flexFlow.sid;
-  console.log('Matching flow chat service SID:', chatServicesSid);
-  console.log('Matching flex flow sid:', flexFlowSid);
+  const chatServiceSid = flexFlow.chatServiceSid
+  const flexFlowSid = flexFlow.sid
+  console.log(`Matching flow chat service SID: ${chatServiceSid} flex flow SID: ${flexFlowSid}`)
 
-  const identity = uuidv1();
+  const identity = uuidv1()
 
-  let chatChannel;
+  let chatChannel
   try {
     chatChannel = await createChatChannelWithTask(
-      context, flexFlowSid, identity, toNumber, toName, fromNumber, workerUri
-    );
+      flexFlowSid,
+      identity,
+      toNumber,
+      toName,
+      fromNumber,
+      workerUri,
+    )
   } catch (error) {
-    response.setStatusCode(error && error.status);
-    response.setBody(error);
-    console.error(error);
-    return callback(null, response);
+    response.setStatusCode(error && error.status)
+    responseBody.error = error
+    response.setBody(responseBody)
+    return callback(null, response)
   }
-  if (!chatChannel) {
-    response.setStatusCode(500);
-    response.setBody({ message: 'Failed to create chat channel' });
-    return callback(null, response);
-  }
-  if (!chatChannel.sid) {
-    response.setStatusCode(chatChannel.status);
-    response.setBody(chatChannel);
-    return callback(null, response);
-  }
-  console.log('Chat channel created:');
-  const responseBody = { chatChannel: { identity } };
-  Object.keys(chatChannel).forEach((key) => {
-    console.log(`${key}: ${chatChannel[key]}`);
-    responseBody.chatChannel[key] = chatChannel[key];
-  });
 
-  let proxySession;
+  responseBody.chatChannel = { identity }
+  Object.keys(chatChannel).forEach((key) => {
+    if (key === '_version' || key === '_solution' || key === '_context') {
+      return
+    }
+    responseBody.chatChannel[key] = chatChannel[key]
+  })
+
+  let proxySession
   try {
     proxySession = await createProxySession(
-      context, chatChannel.sid, toNumber, toName, fromNumber, workerUri
-    );
+      context.TWILIO_PROXY_SERVICE_SID,
+      chatChannel.sid,
+      toNumber,
+      toName,
+      fromNumber,
+    )
   } catch (error) {
-    response.setStatusCode(error && error.status);
-    response.setBody(error);
-    return callback(null, response);
+    response.setStatusCode(error && error.status)
+    responseBody.error = error
+    response.setBody(responseBody)
+    return callback(null, response)
   }
-  if (!proxySession) {
-    response.setStatusCode(500);
-    response.setBody({ message: 'Failed to create proxy session' });
-    return callback(null, response);
-  }
-  if (!proxySession.sid) {
-    response.setStatusCode(proxySession.status);
-    response.setBody(proxySession);
-    return callback(null, response);
-  }
-  console.log('Proxy session created:');
-  responseBody.proxySession = {};
+
+  responseBody.proxySession = {}
   Object.keys(proxySession).forEach((key) => {
-    if (key === '_version' || key === '_solution') {
-      return;
+    if (key === '_version' || key === '_solution' || key === '_context') {
+      return
     }
-    console.log(`${key}: ${proxySession[key]}`);
-    responseBody.proxySession[key] = proxySession[key];
-  });
-  
-  let chatMessage;
-  console.log("Starting Chat Message");
-  console.log(chatChannel.sid);
-  console.log(event.message);
-  console.log(context);
+    responseBody.proxySession[key] = proxySession[key]
+  })
+
+  let chatMessage
   try {
-    chatMessage = await createMessage(
-      context, chatChannel.sid, toNumber, toName, fromNumber, workerUri, event.message
-    );
+    console.log(
+      'starting createChatMessage ' + chatServiceSid + ' ' + chatChannel.sid + ' ' + event.message,
+    )
+    chatMessage = await createChatMessage(chatServiceSid, chatChannel.sid, event.message)
   } catch (error) {
-    response.setStatusCode(error && error.status);
-    response.setBody(error);
-    return callback(null, response);
+    console.log('error creating chat message ' + JSON.stringify(error))
+    response.setStatusCode(error && error.status)
+    responseBody.error = error
+    response.setBody(responseBody)
+    return callback(null, response)
   }
-  if (!chatMessage) {
-    response.setStatusCode(500);
-    response.setBody({ message: 'Failed to add message' });
-    return callback(null, response);
-  }
-  console.log('Message added to channel')
+
+  response.chatMessage = {}
   Object.keys(chatMessage).forEach((key) => {
-    if (key === '_version' || key === '_solution') {
-      return;
+    if (key === '_version' || key === '_solution' || key === '_context') {
+      return
     }
-    console.log(`${key}: ${chatMessage[key]}`);
-    responseBody.chatMessage[key] = chatMessage[key];
-  });
+    responseBody.chatMessage[key] = chatMessage[key]
+  })
 
-  response.setBody(responseBody);
-  return callback(null, response);
-
-});
+  response.setBody(responseBody)
+  return callback(null, response)
+})
